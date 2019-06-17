dd9e232e242897c87c54b504da436b19ad4a950b
shun-itsuki <it_shun_1125@yahoo.co.jp>
2019-06-17 06:00:42 +0000

    add_ranking_function_and_recommendation_article_function

--- app/controllers/users/articles_controller.rb
+++ app/controllers/users/articles_controller.rb
@@ -3,6 +3,8 @@ class Users::ArticlesController < ApplicationController
   def index
     @articles = Article.all.page(params[:page]).per(3)
     # binding.pry
+    @like_ranking = Article.where(created_at:1.month.ago..Time.now).order(likes_count: "DESC").limit(4)
+    @recommendations = Article.order("RANDOM()").limit(4)
   end
 
   def new
